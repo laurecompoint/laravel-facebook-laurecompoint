3c4d5f0e33bcfcb6f78b4ba516fea85bd85de39e
Laure C <compointlaure@gmail.com>
2020-03-21 17:58:07 +0100

    update page index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,14 +14,13 @@
                     <div class="">
                         <img src="img/avatar.png" class="ml-1" style="width: 40PX"/>
                     </div>
+                  
 
                     <div class="ml-2 col-11">
                       
-                    <input type="text" class="form-control inputpost" placeholder="Que voulez-vous dire, {{ Auth::user()->name }} ?" name="post" />
+                    <input type="text" data-emoji-picker="true" class="form-control inputpost" placeholder="Que voulez-vous dire, {{ Auth::user()->name }} ?" name="post" />
                     </div>
-
-                         
-                        
+    
                   </div>
                   {{csrf_field()}}
                   <button type="summit" class="btn col-11 text-white mt-3 color">Publier</button>
@@ -54,7 +53,6 @@
             <div class="card mt-5">
                 <div class="card-header bg-white">
 
-               
                   <div class="row m-auto">
                     <div class="col-1">
                         <img src="img/{{$post->user->avatar}}" class="ml-1" style="width: 40PX; border-radius: 10px 100px / 120px;"/>
@@ -79,16 +77,12 @@
                          
                   </div>
                
-
-
                 </div>
 
                 <div class="card-body" style="background-color: rgba(0,0,0,.03);">
 
                 <p>{{$post->post}}</p>
                
-
-                       
 
                         <div class="row float-right mr-5 col-10">
                         @foreach ( $post->reply()->get() as $replypost  )
@@ -107,9 +101,6 @@
                          
                         </div>
                       
-
-              
-
                 <form method="post" action="{{route('replys.create')}}">
                 <input type="hidden" class="form-control" name="post_id" value="{{$post->id}}" placeholder="" aria-label="" aria-describedby="basic-addon1">
                 <input type="hidden" class="form-control" name="user_id" value="{{Auth::user()->id}}" placeholder="test" aria-label="" aria-describedby="basic-addon1">
