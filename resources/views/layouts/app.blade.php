970fe24524481f62516f639d4580c1aa1373df7e
Laure C <compointlaure@gmail.com>
2020-03-25 16:10:40 +0100

    alerte supression modal ok pour supression amis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,14 @@
     <!-- Scripts -->
    
    
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     
-
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -103,9 +103,36 @@
                                 @if($is_edit_profile) 
                               
                               
-                                <button type="button" class="btn text-danger col-6  mr-5"  data-toggle="modal" data-target="#Modalalerte">
+                                <button type="button" class="btn text-danger col-6  mr-5"  data-toggle="modal" data-target="#Modalalerte{{ $friends->id }}">
                                 Remove friend
-                        </button>   
+                               </button>  
+
+                               <div class="modal fade " id="Modalalerte{{ $friends->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog " role="document">
+                            <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Supression alerte ?</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <h6>Etês vous sur de vouloir supprimer votre amitier avec : " {{$friends->name}} " ?</h6>
+                                    <p>Vous ne serez plus jamais amis!!!</p>
+
+                                    </div>
+                                    <div class="modal-footer">
+                                    
+                                    <a href="{{ url('/remove-friends/' . $userprofil->name) }}" class="">
+                                    <button type="button" class="btn text-danger col-12 mb-1 ">Remove friend</button>
+                                
+                                </a>
+                                    </div>
+                                </div>     
+                            </div>  
+                         
+                         </div>
+           
                                 @else 
 
                                 @endif 
@@ -220,31 +247,6 @@
 
 </div>
 
-<div class="modal fade " id="Modalalerte" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog " role="document">
-                    <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title text-danger" id="exampleModalLabel">Supression alerte</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>
-                            <div class="modal-body">
-                            <h6>Etês vous sur de vouloir supprimer votre amitier ?</h6>
-                            <p>Vous ne serez desormais plus jamais amis!!! </p>
-                        
-                            </div>
-                            <div class="modal-footer">
-                            
-                            <a href="{{ url('/remove-friends/' . $userprofil->name) }}" class="">
-                                    <button type="button" class="btn text-danger col-12 mb-1 ">Remove friend</button>
-                                
-                                </a>
-                            </div>
-                        </div>     
-                    </div>
-               
- </div>
 
  
 
